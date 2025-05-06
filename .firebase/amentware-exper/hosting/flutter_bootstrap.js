c7f2acb275219c414b1421be70122f1bedfc70f8
amentware <amentware@gmail.com>
2025-05-06 18:53:45 +0000

    deplyed

--- .firebase/amentware-exper/hosting/flutter_bootstrap.js
+++ .firebase/amentware-exper/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"2ba456fd7fd03d7348f9fa0952493030c85f15
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3088492249"
+    serviceWorkerVersion: "3390259231"
   }
 });
